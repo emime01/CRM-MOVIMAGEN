38715204ef1c3fbf18ab9db211c57a2c4244c6f1
emime01 <emiliano@movimagen.com>
2026-04-15 17:31:55 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,24 +10,6 @@ module.exports = {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
       },
-      colors: {
-        orange: {
-          DEFAULT: '#eb691c',
-          light:   '#f07f3c',
-          pale:    '#fdf0e8',
-          deep:    '#c45a10',
-        },
-        gray: {
-          100: '#f4f3f0',
-          200: '#e8e6e1',
-          400: '#b0aba3',
-          600: '#6e6a62',
-          800: '#2e2b26',
-        },
-      },
-      borderRadius: {
-        DEFAULT: '10px',
-      },
     },
   },
   plugins: [],
